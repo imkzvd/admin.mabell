8151348b6ae7cee80a1fc92b72e49d6674da6478
imkzvd <imkzvd@gmail.com>
2025-05-06 23:36:18 +0300

    feat: added "UISwitch" component
    Refs: #20

---
+++ src/shared/components/presenters/UI/UISwitch/UISwitch.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-switch
+    density="compact"
+    class="ui-switch"
+    :ripple="false"
+    :messages="notes"
+    :label="label"
+    :color="color"
+    :disabled="isDisabled"
+    :model-value="modelValue"
+    @update:model-value="emit('update:modelValue', !!$event)"
+  />
+</template>
+
+<script setup lang="ts">
+import type { UISwitchEmits, UISwitchProps } from './types';
+
+withDefaults(defineProps<UISwitchProps>(), {
+  color: 'success',
+});
+const emit = defineEmits<UISwitchEmits>();
+</script>
+
+<style scoped lang="scss"></style>
