7d77d6404e02a575ff537a0d4947d16b50afe0e0
imkzvd <imkzvd@gmail.com>
2025-05-06 19:27:07 +0300

    feat: add global component config in vite config
    Refs: #23

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
 import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,6 +14,12 @@ export default defineConfig({
     AutoImport({
       imports: ['vue', 'vue-router', 'pinia'],
       dts: 'src/auto-imports.d.ts',
+    }),
+    Components({
+      dirs: ['src'],
+      deep: true,
+      extensions: ['vue'],
+      dts: 'src/components.d.ts',
     }),
   ],
   resolve: {
