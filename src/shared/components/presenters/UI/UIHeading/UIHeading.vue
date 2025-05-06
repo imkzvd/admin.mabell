3d910fa92c8b3f8a7b293e0ec349260305d518a3
imkzvd <imkzvd@gmail.com>
2025-05-06 23:07:10 +0300

    feat: added "UIHeading" component
    Refs: #20

---
+++ src/shared/components/presenters/UI/UIHeading/UIHeading.vue
@@ -0,0 +1,43 @@
+<template>
+  <component :is="'h' + level" class="ui-heading" :class="cssClasses">
+    <slot />
+  </component>
+</template>
+
+<script setup lang="ts">
+import type { UIHeadingProps } from './types';
+
+const props = withDefaults(defineProps<UIHeadingProps>(), {
+  level: 1,
+});
+
+const cssClasses = computed<Record<string, string | number | boolean>>(() => ({
+  'leading-none': props.leadingNone,
+}));
+</script>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 32px !important;
+  font-weight: 400 !important;
+  font-family: CircularSpTitleBold, CircularSpCyrlBold, sans-serif;
+}
+
+h2 {
+  font-size: 20px;
+  font-family: CircularSpTitleBold, CircularSpCyrlBold, sans-serif;
+  /*font-weight: 400 !important;*/
+  /*font-family: CircularSpTitleBold, sans-serif;*/
+}
+
+h3 {
+  font-size: 16px !important;
+  font-weight: 400 !important;
+  font-family: CircularSpTitleBold, sans-serif;
+}
+
+// TODO: refactor
+.leading-none {
+  line-height: 1;
+}
+</style>
