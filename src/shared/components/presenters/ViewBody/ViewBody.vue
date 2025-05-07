abd2af64c8e6ebd7d2a4d1b147deeaeb1175d2d2
imkzvd <imkzvd@gmail.com>
2025-05-07 19:54:45 +0300

    feat: added "ViewHeader" and "ViewBody" components
    Refs: #17

---
+++ src/shared/components/presenters/ViewBody/ViewBody.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="view-body py-4">
+    <div class="container">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
