f3a7da2fc3b6fe9aca171f95bb69f1b80e31ae7f
imkzvd <imkzvd@gmail.com>
2025-05-10 00:29:55 +0300

    feat: added new item in nav menu items list
    Refs: #47

--- src/shared/components/presenters/NavMenu/constants.ts
+++ src/shared/components/presenters/NavMenu/constants.ts
@@ -3,4 +3,5 @@ import type { NavMenuItem } from './types.ts';
 export const navMenuItems: NavMenuItem[] = [
   { to: { name: 'home' }, title: 'Home' },
   { to: { name: 'admins' }, title: 'Admins' },
+  { to: { name: 'artists' }, title: 'Artists' },
 ];
