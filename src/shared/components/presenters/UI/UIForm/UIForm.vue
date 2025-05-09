d2d09fea02a4b0c4b7ac2ab82fe5135dfa7cb6f6
imkzvd <imkzvd@gmail.com>
2025-05-10 00:19:35 +0300

    fix: delete reset button logic from "UIForm" component
    Refs: #41

--- src/shared/components/presenters/UI/UIForm/UIForm.vue
+++ src/shared/components/presenters/UI/UIForm/UIForm.vue
@@ -1,10 +1,8 @@
 <template>
-  <v-form ref="formInstance" class="ui-form" @submit.prevent="emit('submit', $event)">
+  <v-form class="ui-form" @submit.prevent="emit('submit', $event)">
     <slot name="default" />
 
-    <v-btn type="submit" :ripple="false" color="primary" class="mr-2">Submit</v-btn>
-
-    <v-btn :ripple="false" color="error" @click="onResetButtonClick">Reset</v-btn>
+    <v-btn type="submit" :ripple="false" color="primary">Submit</v-btn>
   </v-form>
 </template>
 
@@ -13,13 +11,6 @@ import type { UIFormEmits } from './types';
 import type { VForm } from 'vuetify/components';
 
 const emit = defineEmits<UIFormEmits>();
-
-const form = ref<VForm | null>(null);
-
-function onResetButtonClick() {
-  form.value?.reset();
-  emit('reset');
-}
 </script>
 
 <style scoped lang="scss"></style>
