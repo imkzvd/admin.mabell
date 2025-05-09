9011f40b7d09a59fd550d14ec63bca932278983a
imkzvd <imkzvd@gmail.com>
2025-05-10 00:01:05 +0300

    feat: prepared admins module routes
    Refs: #17

---
+++ src/features/admins/routes/index.ts
@@ -0,0 +1,21 @@
+import UISpinner from '@/shared/components/presenters/UI/UISpinner/UISpinner.vue';
+import type { RouteRecordRaw } from 'vue-router';
+
+export const routes: RouteRecordRaw[] = [
+  {
+    path: '/admins',
+    name: 'admins',
+    component: defineAsyncComponent({
+      loader: () => import('../views/AdminsView.vue'),
+      loadingComponent: () => h(UISpinner, { isCentered: true }),
+    }),
+  },
+  {
+    path: '/admin/:id',
+    name: 'admin',
+    component: defineAsyncComponent({
+      loader: () => import('../views/AdminView.vue'),
+      loadingComponent: () => h(UISpinner, { isCentered: true }),
+    }),
+  },
+];
