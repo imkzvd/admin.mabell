c7e28deb083e655ac160310622b5c8607ee0a485
imkzvd <imkzvd@gmail.com>
2025-05-10 00:33:22 +0300

    feat: added "ArtistView" and "ArtistsView" components
    Refs: #47

---
+++ src/features/artists/views/ArtistView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="artist-view">
+    <h1>Artist View</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/features/artists/views/ArtistsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="artists-view">
+    <h1>Artists View</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
