d2a38e4570077d4be436aff4943527f9d25fdff9
imkzvd <imkzvd@gmail.com>
2025-05-12 22:09:26 +0300

    feat: added "AlbumView" and "AlbumsView" components
    Refs: #49

---
+++ src/features/albums/views/AlbumView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="album-view">
+    <ViewHeader class="album-view__header">
+      <h1>Album View</h1>
+    </ViewHeader>
+
+    <ViewBody class="album-view__body"></ViewBody>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
