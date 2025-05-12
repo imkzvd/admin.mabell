d08ef136cdf5f8028638a6016bcd0caf9fa8d573
imkzvd <imkzvd@gmail.com>
2025-05-12 16:01:51 +0300

    feat: updated "UIColorPicker" component
    * added action reset and delete buttons
    * set string hex mode
    * updated styles and types
    
    Refs: #47

--- src/shared/components/presenters/UI/UIColorPicker/types.ts
+++ src/shared/components/presenters/UI/UIColorPicker/types.ts
@@ -1,9 +1,13 @@
+import type UIColorPicker from '@/shared/components/presenters/UI/UIColorPicker/UIColorPicker.vue';
+
 export type UIColorPickerProps = {
   modelValue: string | null;
   label?: string;
-  width?: string | number;
+  width?: string;
 };
 
 export type UIColorPickerEmits = {
   (e: 'update:modelValue', value: string | null): void;
 };
+
+export type UIColorPickerInstance = InstanceType<typeof UIColorPicker>;
