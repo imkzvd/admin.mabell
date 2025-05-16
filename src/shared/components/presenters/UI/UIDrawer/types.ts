b64026f290d23d547a9ad0f5886420eb8e40deb2
imkzvd <imkzvd@gmail.com>
2025-05-16 15:02:10 +0300

    feat: updated "UIDrawer" component:
    * added new events "opened" and "closes"
    * added "isLoading" prop
    * added custom styles for vuetify "VNavigationDrawer" component
    
    Refs: #52

--- src/shared/components/presenters/UI/UIDrawer/types.ts
+++ src/shared/components/presenters/UI/UIDrawer/types.ts
@@ -1,8 +1,11 @@
 export type UIDrawerProps = {
   modelValue?: boolean;
   width?: number;
+  isLoading?: boolean;
 };
 
 export type UIDrawerEmits = {
   (e: 'update:modelValue', value: boolean): void;
+  (e: 'opened'): void;
+  (e: 'closed'): void;
 };
