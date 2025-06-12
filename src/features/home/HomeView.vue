e0811c5f711d53a6802cabe3e3a8e4cf2506fd97
imkzvd <imkzvd@gmail.com>
2025-06-12 21:39:39 +0300

    feat: added global search in home page
    Refs: #56

--- src/features/home/HomeView.vue
+++ src/features/home/HomeView.vue
@@ -1,7 +1,71 @@
 <template>
-  <h1>Home View</h1>
+  <div class="home-view">
+    <ViewHeader class="home-view__header">
+      <div class="home-view__header-content">
+        <UIChip icon="mdi-plus">Create</UIChip>
+
+        <UIChip icon="mdi-account">Profile</UIChip>
+      </div>
+    </ViewHeader>
+
+    <ViewBody class="home-view__body">
+      <div class="home-view__body-content">
+        <UIHeading class="mb-4 home-view__heading">Search</UIHeading>
+
+        <GlobalSearch class="home-view__global-search" />
+      </div>
+    </ViewBody>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-view {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+
+  &__header {
+    margin-bottom: 240px;
+  }
+
+  &__header-content {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  &__body {
+    overflow: hidden;
+  }
+
+  &__body-content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    max-width: 800px;
+    margin-inline: auto;
+  }
+
+  &__search-input {
+    margin-inline: auto;
+  }
+
+  &__nav-menu {
+    max-width: 200px;
+    margin-inline: auto;
+    width: fit-content;
+  }
+
+  &__heading {
+    text-align: center;
+    font-size: 40px !important;
+  }
+
+  &__global-search {
+    height: 100%;
+    overflow: auto;
+  }
+}
+</style>
