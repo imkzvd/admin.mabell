67972c73f0be9ccf5de6406aa10ce6ca7f68feb7
imkzvd <imkzvd@gmail.com>
2025-06-13 00:29:40 +0300

    feat: added "UIMenu" component
    Refs: #58

---
+++ src/shared/components/presenters/UI/UIMenu/UIMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-menu :close-on-content-click="false" scrim>
+    <template v-slot:activator="{ props }">
+      <slot name="activator" :activator="props" />
+    </template>
+
+    <v-card min-width="240">
+      <slot name="menu" />
+    </v-card>
+  </v-menu>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
