390ec53adb687730d2aaaa04354521387f2c0dfa
imkzvd <imkzvd@gmail.com>
2025-06-14 21:07:03 +0300

    feat: added "UserAvatarForm" component
    Refs: #60

---
+++ src/features/users/components/presenters/UserAvatarForm/UserAvatarForm.vue
@@ -0,0 +1,60 @@
+<template>
+  <UIForm :is-loading="isLoading" class="user-avatar-form" @submit="onSubmitForm">
+    <div class="user-avatar-form__content">
+      <ImageUploader
+        ref="imageUploaderInstance"
+        width="300px"
+        height="300px"
+        is-rounded
+        :preview-url="user.avatar"
+        v-model="formState.fileId"
+      />
+
+      <UIColorPicker ref="colorPickerInstance" width="300px" v-model="formState.color" />
+    </div>
+  </UIForm>
+</template>
+
+<script lang="ts" setup>
+import type {
+  UserAvatarFormEmits,
+  UserAvatarFormProps,
+  UserAvatarFormState,
+} from '@/features/users/components/presenters/UserAvatarForm/types.ts';
+import type { ImageUploaderInstance } from '@/shared/components/containers/ImageUploader/types.ts';
+import type { UIColorPickerInstance } from '@/shared/components/presenters/UI/UIColorPicker/types.ts';
+
+const props = defineProps<UserAvatarFormProps>();
+const emit = defineEmits<UserAvatarFormEmits>();
+
+const imageUploaderInstance = ref<ImageUploaderInstance | null>(null);
+const colorPickerInstance = ref<UIColorPickerInstance | null>(null);
+
+const formState = reactive<UserAvatarFormState>({
+  fileId: null,
+  color: props.user.color,
+});
+
+function resetState() {
+  imageUploaderInstance.value?.resetState();
+  colorPickerInstance.value?.resetState();
+}
+
+function onSubmitForm() {
+  emit('submit', formState);
+}
+
+defineExpose({
+  resetState,
+});
+</script>
+
+<style lang="scss" scoped>
+.user-avatar-form {
+  &__content {
+    display: flex;
+    align-items: center;
+    column-gap: 32px;
+  }
+}
+</style>
