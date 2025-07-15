41bf4ef8c94cd3d5fa80087922c73447093cd6e8
imkzvd <imkzvd@gmail.com>
2025-07-15 21:33:56 +0300

    feat: added "EmptyLayout" component + background image
    Refs: #66

---
+++ src/layouts/EmptyLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="empty-layout">
+    <main class="empty-layout__main">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.empty-layout {
+  //position: relative;
+  min-height: 100vh;
+  background: url('@/assets/images/desktop-layout-bg.png') no-repeat;
+  background-size: cover;
+
+  &::before {
+    position: absolute;
+    inset: 0;
+    content: '';
+    background: var(--overlay-color);
+    backdrop-filter: blur(40px) saturate(500%);
+  }
+
+  &__main {
+    position: relative;
+    z-index: 1;
+    min-height: 100vh;
+  }
+}
+</style>
