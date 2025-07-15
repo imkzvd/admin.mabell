5e7d12e1ca49030e590389a379125216ab10911e
imkzvd <imkzvd@gmail.com>
2025-07-15 21:46:55 +0300

    feat: moved fetch metadata from "App" component to "AuthorizationView" component
    Refs: #66

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,6 @@
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import { useMetadataStore } from '@/features/metadata/stores/metadata.store.ts';
-
-const metadataStore = useMetadataStore();
-
-onMounted(() => metadataStore.fetchMetadata());
 </script>
 
 <style scoped lang="scss"></style>
