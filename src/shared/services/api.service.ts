6c4dedbfd81a5c5cd5ac2859ed041dd6fe208605
imkzvd <imkzvd@gmail.com>
2025-07-18 01:24:39 +0300

    feat: added custom fetch for refresh token in auth service
    Refs: #66

--- src/shared/services/api.service.ts
+++ src/shared/services/api.service.ts
@@ -1,8 +1,45 @@
 import { ApiModule } from '@/api/api.module';
 
+let isRefreshing = false;
+
 export const apiService = new ApiModule({
   baseUrl: import.meta.env.VITE_ADMIN_API_URL,
   baseApiParams: {
     credentials: 'include',
+  },
+  customFetch: async (path, options) => {
+    try {
+      const response = await fetch(path, options);
+      const { pathname } = new URL(path as string);
+
+      if (
+        response.status !== 401 ||
+        pathname === '/auth/login' ||
+        pathname === '/auth/refresh' ||
+        isRefreshing
+      ) {
+        return response;
+      }
+
+      try {
+        isRefreshing = true;
+
+        const { ok, error } = await apiService.auth.refreshAccessToken();
+
+        if (!ok) {
+          throw error;
+        }
+
+        return fetch(path, options);
+      } catch (e) {
+        throw e;
+      } finally {
+        isRefreshing = false;
+      }
+
+      return response;
+    } catch (e) {
+      throw e;
+    }
   },
 });
