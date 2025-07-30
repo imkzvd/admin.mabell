546992134ad7c4092bc76a371a753cb3fb44b5ba
imkzvd <imkzvd@gmail.com>
2025-07-30 16:47:42 +0300

    feat: added shared "NotFoundView" component + updated router config
    Refs: #68

---
+++ src/shared/views/NotFoundView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="not-found-view">
+    <ViewBody>
+      <UIHeading>404</UIHeading>
+
+      <UIText color="secondary">Not found page</UIText>
+    </ViewBody>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
