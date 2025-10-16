2bda7848a7a96a545d704e4442738ca97e2ac14b
imkzvd <imkzvd@gmail.com>
2025-10-16 20:42:44 +0300

    feat: added version page
    Closes: #100

---
+++ src/shared/views/VersionView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="version-view">
+    <ViewBody>
+      <UIText color="secondary">v{{ version }}</UIText>
+    </ViewBody>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { version } from '../../../package.json';
+</script>
+
+<style scoped lang="scss"></style>
